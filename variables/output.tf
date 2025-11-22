57d9fcc16735b3d84f23fe82df6e6cf7cb045131
Guru Theja <gurutejashetty@gmail.com>
2025-11-22 10:23:22 +0530

    Auto commit

--- variables/output.tf
+++ variables/output.tf
@@ -2,6 +2,14 @@ variable "sample" {
     default = "Hello Guru, Welcome to Terraform learning"
 }
 
+variable "sample1" {
+    default = "Hello Guru, Welcome to Terraform variables concept"
+}
+
 output "print_sample" {
     value = var.sample
+}
+
+output "print_sample1" {
+    value = "The value of variable sample1 is ${var.sample1}"
 }
